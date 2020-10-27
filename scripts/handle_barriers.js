b9bd0b0df3f461009331872a79542d731c37e57e
montheramer <monther.amer48@gmail.com>
2020-10-27 20:40:46 +0300

    ADD animation to barrier

---
+++ scripts/handle_barriers.js
@@ -0,0 +1,17 @@
+let barrierInterval;
+const container = document.querySelector('.container');
+const barrier = document.createElement('div');
+
+const moveBarrier = () => {
+  container.appendChild(barrier);
+  let barrierNumber = Math.floor(Math.random() * Math.floor(7)) + 1;
+  barrier.classList.remove('barrier1', 'barrier2', 'barrier3', 'barrier4', 'barrier5', 'barrier6', 'barrier7');
+  barrier.classList.add('barrier', `barrier${barrierNumber}`);
+};
+
+export const createBarriers = () => {
+  moveBarrier();
+  barrierInterval = barrierInterval = setInterval(() => {
+    moveBarrier();
+  }, 3700);
+};
