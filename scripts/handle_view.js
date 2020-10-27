2be29d7c570734d72ab3141c0116dc02ede95b3d
montheramer <monther.amer48@gmail.com>
2020-10-27 23:31:06 +0300

    ADD cloud to  background

--- scripts/handle_view.js
+++ scripts/handle_view.js
@@ -1,10 +1,20 @@
 export const startView = () => {
-  const dinosaur = document.querySelector('.dinosaur');
-  const ground = document.createElement('div');
-  const ground2 = document.createElement('div');
+  const container = document.querySelector('.container'),
+    ground = document.createElement('div'),
+    ground2 = document.createElement('div'),
+    cloud1 = document.createElement('div'),
+    cloud2 = document.createElement('div'),
+    cloud3 = document.createElement('div');
+
+  cloud1.classList.add('cloud', 'move-cloud-1');
+  cloud2.classList.add('cloud', 'move-cloud-2');
+  cloud3.classList.add('cloud', 'move-cloud-3');
   ground.classList.add('ground');
   ground2.classList.add('ground2');
 
-  dinosaur.appendChild(ground);
-  dinosaur.appendChild(ground2);
+  container.appendChild(ground);
+  container.appendChild(ground2);
+  container.appendChild(cloud1);
+  container.appendChild(cloud2);
+  container.appendChild(cloud3);
 };
