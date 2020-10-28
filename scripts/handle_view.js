cf5ed070a0c6f6a575f68a748fed0bc623f38a4a
montheramer <monther.amer48@gmail.com>
2020-10-28 12:26:40 +0300

    add title

--- scripts/handle_view.js
+++ scripts/handle_view.js
@@ -4,6 +4,7 @@ export const startView = () => {
     ground2 = document.createElement('div'),
     cloud1 = document.createElement('div'),
     cloud2 = document.createElement('div'),
+    score = document.querySelector('.score_container'),
     cloud3 = document.createElement('div');
 
   cloud1.classList.add('cloud', 'move-cloud-1');
@@ -11,7 +12,7 @@ export const startView = () => {
   cloud3.classList.add('cloud', 'move-cloud-3');
   ground.classList.add('ground');
   ground2.classList.add('ground2');
-
+  score.style.display = 'flex';
   container.appendChild(ground);
   container.appendChild(ground2);
   container.appendChild(cloud1);
